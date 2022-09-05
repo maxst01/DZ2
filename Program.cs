7185f464cf0286f0d8358d318a892965c9d9de8d
Assauit <maxst01@list.ru>
2022-09-05 10:52:05 +0700

    Готовое дз 2

--- Program.cs
+++ Program.cs
@@ -1,1 +1,48 @@
-﻿
+﻿// Задача 10
+//internal class Program
+//{
+//    private static void Main(string[] args)
+//    {
+//        
+//        Console.WriteLine("введите трехзначное число");
+//        int a = Convert.ToInt32(Console.ReadLine());
+//        if (a < 1000 && x > 99)
+//{
+//        int b = a / 10;
+//        int result = b % 10;
+//        Console.Write(result);
+//        }
+//}
+//}
+
+
+// Задача 13
+//Console.WriteLine("введите трехзначное число");
+//int a = Convert.ToInt32(Console.ReadLine());
+//if (a < 1000 && a > 99)
+//{
+//int b = a / 10;
+//int result = b % 10;
+//Console.Write(result);
+//}
+//else
+//Console.WriteLine("ввели не трехзначное число");
+
+
+//Задача 15
+//Console.WriteLine("введите цифру дня недели");
+//int a = Convert.ToInt32(Console.ReadLine());
+//if (a > 0 && a < 6)
+//{
+//    Console.Write("Нет");
+//}
+//
+//if (a > 5 && a < 8)
+//{
+//    Console.Write("Да");
+//}
+//
+//if (a < 1 || a > 7)
+//{
+//    Console.Write("Ввели не корректный день недели");
+//}
